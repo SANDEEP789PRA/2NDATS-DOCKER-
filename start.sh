7622bd2b3392662e2b23e78b3a98a19a0789a645
sandeep prajapati <sandeepprajapati789789@gmail.com>
2025-09-27 13:36:07 +0530

    interigate cicd

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 
 # Start Flask app with Gunicorn (production server) in background
-gunicorn --bind 0.0.0.0:8000 server:app &# Run Flask server in background
+gunicorn --bind 0.0.0.0:8000 server:app & # Run Flask server in background
 
 
 # Run Streamlit app
